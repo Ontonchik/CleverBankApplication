b085ec943e1f4b58e7817e2fb18b594fcd8b2370
Anton Chigir <antonchigir@gmail.com>
2023-09-03 19:52:50 +0300

    Added use of .yml Also with the help of .yml fixed bag with checkNumber. Before that every check had number 1.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,8 @@
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.start();
-
-//        View view = new View();
-//        Transaction testTransaction = new Transaction(new Account(123, new Bank("Clever-bank")),
-//                new Account(516, new Bank("Bebra-bank")),new BigDecimal(518),"Перевод");
-//        view.printCheck(testTransaction);
-//        System.out.println(LocalTime.now().getHour());
+        Thread thread = new Thread(controller);
+        thread.start();
+        controller.begin();
     }
 }
