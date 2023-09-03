15b7864dcd3c2d3a299cc6e818e4a5d34e84871d
Anton Chigir <antonchigir@gmail.com>
2023-09-04 01:21:16 +0300

    Generated JavaDoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Account"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Bank"},{"p":"<Unnamed>","l":"Controller"},{"p":"<Unnamed>","l":"Dao"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Transaction"},{"p":"<Unnamed>","l":"User"},{"p":"<Unnamed>","l":"View"}];updateSearchResults();
